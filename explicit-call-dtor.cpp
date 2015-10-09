aab2c9f1c641462f80a6697e2826ace68b2e7542
JumsphenmXu <xuxh13@163.com>
2015-10-09 18:57:55 +0800

    show how dtor can be called in c++(malloc, free is also ok)

--- explicit-call-dtor.cpp
+++ explicit-call-dtor.cpp
@@ -13,11 +13,14 @@ int main() {
 	/*
 	 *	Only in the way when 
 	 *	::operator new(size_t size) + placement new + ::operator delete(void *)
-	 *	called, then dtor can be explicitly called by programmers
+	 *	called, then dtor can be explicitly called by programmers.
+	 *	Substituting ::operator new(size_t) by malloc(size_t) and accordingly
+	 *	::operator delete(void *) by free(void *) is also applicable.
 	 */
 
 	// ::operator new(size_t size) allocate the raw memory
 	ExplicitDtor *ped = static_cast<ExplicitDtor *>(::operator new(sizeof(ExplicitDtor)));
+	// ExplicitDtor *ped = static_cast<ExplicitDtor *>(malloc(sizeof(ExplicitDtor)));
 
 	// placement new to construct the ExplicitDtor object
 	new (ped) ExplicitDtor();
@@ -27,6 +30,7 @@ int main() {
 
 	// deallocate the raw memory which was allocated by ::operator new(size_t size)
 	::operator delete(ped);
+	// free(ped);
 
 
 	return 0;
