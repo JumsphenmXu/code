d1c084c769b70d1bf58cde9101d93c5ef6947481
xuxh13 <xuxh13@163.com>
2016-02-25 21:43:51 +0800

    add budgeted class

---
+++ g/bcim/XBudgetedCIM.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+__author__ = 'XU Xinhui'
+
+
+class XBudgetedCIM(object):
+	def __init__(self):
+		pass
+
+	def greedy(self, graph, T, budget):
+		pass
+
+	def degree_heuristic(self, graph, T, budget):
+		pass
+
+	def comparable_heuristic(self, graph, T, budget):
+		pass
